4d07dce0ffa7ce38d7c60a5ce5f7cb0ee809cf66
Developer Kush 👨🏼‍💻 || Full stack web developer 💪🏼 <kushpandit68775@gmail.com>
2026-05-30 17:24:30 +0530

    route added for donation and notfound

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -13,28 +13,34 @@ import Services from './pages/Services.jsx'
 import SupportUs from './pages/SupportUs.jsx'
 import Team from './pages/Team.jsx'
 import Volunteer from './pages/Volunteer.jsx'
+import Donation from './pages/Donation.jsx'
+import NotFound from './pages/NotFound.jsx'
 import ScrollAnimationProvider from './components/ScrollAnimationProvider.jsx'
 
 function App() {
   return (
     <ScrollAnimationProvider>
-    <Routes>
-      <Route element={<Layout />}>
-        <Route index element={<Home />} />
-        <Route path="about" element={<About />} />
-        <Route path="contact" element={<Contact />} />
-        <Route path="emergency" element={<Emergency />} />
-        <Route path="facilities" element={<Facilities />} />
-        <Route path="faq" element={<FAQ />} />
-        <Route path="financial-transparency" element={<FinancialTransparency />} />
-        <Route path="gallery" element={<Gallery />} />
-        <Route path="impact" element={<Impact />} />
-        <Route path="services" element={<Services />} />
-        <Route path="support-us" element={<SupportUs />} />
-        <Route path="team" element={<Team />} />
-        <Route path="volunteer" element={<Volunteer />} />
-      </Route>
-    </Routes>
+      <Routes>
+        <Route element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="about" element={<About />} />
+          <Route path="contact" element={<Contact />} />
+          <Route path="emergency" element={<Emergency />} />
+          <Route path="facilities" element={<Facilities />} />
+          <Route path="faq" element={<FAQ />} />
+          <Route path="financial-transparency" element={<FinancialTransparency />} />
+          <Route path="gallery" element={<Gallery />} />
+          <Route path="impact" element={<Impact />} />
+          <Route path="services" element={<Services />} />
+          <Route path="support-us" element={<SupportUs />} />
+          <Route path="team" element={<Team />} />
+          <Route path="volunteer" element={<Volunteer />} />
+          {/* ✅ Fixed: Donation page now exists */}
+          <Route path="donation" element={<Donation />} />
+          {/* ✅ Fixed: 404 catch-all */}
+          <Route path="*" element={<NotFound />} />
+        </Route>
+      </Routes>
     </ScrollAnimationProvider>
   )
 }
